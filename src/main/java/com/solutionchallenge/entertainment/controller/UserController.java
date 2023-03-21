1a9e9290e90c420145615cb822563168d00b8f3d
solhee-hwang <solhee0912@kookmin.ac.kr>
2023-03-21 13:32:15 +0900

    fix : crossorgin error complete

--- src/main/java/com/solutionchallenge/entertainment/controller/UserController.java
+++ src/main/java/com/solutionchallenge/entertainment/controller/UserController.java
@@ -9,16 +9,14 @@ import com.solutionchallenge.entertainment.service.SeniorService;
 import com.solutionchallenge.entertainment.service.TutorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth/signin")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class UserController {
     private final SeniorService seniorService;
     private final GuardianService guardianService;
